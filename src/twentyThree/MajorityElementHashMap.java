430f5c1c004c65f14c5c493077cd55679b49d124
Colin Mills <colinm@indeed.com>
2024-01-22 21:00:14 -0700

    add HashMap problem

---
+++ src/twentyThree/MajorityElementHashMap.java
@@ -0,0 +1,17 @@
+package twentyThree;
+
+public class MajorityElementHashMap {
+        public int majorityElement(int[] nums) {
+            // First thought is using a hash map to store frequencies and if any of the frequencies
+            // exceed n/2 then we break and report the value
+            HashMap<Integer, Integer> numFrequency = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int currentFrequency = numFrequency.getOrDefault(nums[i], 0);
+                if (currentFrequency + 1 > nums.length / 2) {
+                    return nums[i];
+                }
+                numFrequency.put(nums[i], currentFrequency + 1);
+            }
+            return -1;
+        }
+}
