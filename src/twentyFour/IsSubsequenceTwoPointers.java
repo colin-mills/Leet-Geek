07968519fc975778f7d931aa2e3cb48aa74a04bd
Colin Mills <colinm@indeed.com>
2024-02-10 08:27:28 -0700

    better solution

--- src/twentyFour/IsSubsequenceTwoPointers.java
+++ src/twentyFour/IsSubsequenceTwoPointers.java
@@ -8,27 +8,26 @@ public class IsSubsequenceTwoPointers {
         // match for the current sChar
         // Return true if we found a match for all of s, else
         // if we reached the end of t, we didn't find a substring
+
+        // Edge case
+        if (s.length() == 0) return true;
+
         int sChar = 0;
         for (int tChar = 0; tChar < t.length(); tChar++) {
-            // If we scanned all of sChar then we have a match and an early exit opportunity
-            if (sChar == s.length()) {
-                return true;
-            }
-
             if (s.charAt(sChar) == t.charAt(tChar)) {
                 // Found a match walk sChar forward
                 sChar++;
             }
-        }
 
-        // If we scanned all of sChar then we have a match
-        if (sChar == s.length()) {
-            return true;
+            // If we scanned all of sChar then we have a match and an early exit opportunity
+            if (sChar == s.length()) {
+                return true;
+            }
         }
 
         return false;
 
-        //Time complexity is O(s+t) where s is the length of s and t is the length of t
+        //Time complexity is O(s+t) where s is the lenght of s and t is the length of t
         //Space complexity is O(1) as this can be done with constant scratch space
     }
 }
